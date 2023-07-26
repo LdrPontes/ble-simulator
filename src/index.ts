0b94b4f4e2765e4d8d3ab153f4eace0d34256ef7
Leandro Pontes Berleze <leandropberleze@gmail.com>
2023-07-25 23:35:44 -0300

    feat: add services

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,14 @@ bleno.on('stateChange', function(state) {
 
   try {
     if (state === 'poweredOn') {
-      bleno.startAdvertising('MyRPI-Simulator', ['1803']);
+      var name = 'name';
+      var serviceUuids = ['fffffffffffffffffffffffffffffff0']
+      
+      bleno.startAdvertising(name, serviceUuids, function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
       console.log('startAdvertising');
     } else {
       bleno.stopAdvertising();
@@ -13,5 +20,30 @@ bleno.on('stateChange', function(state) {
     }
   } catch (error) {
     console.log(error);
+  }
+});
+
+bleno.on('advertisingStart', function(err) {
+  if (!err) {
+    console.log('advertising...');
+
+    bleno.setServices([
+      new bleno.PrimaryService({
+        uuid: 'fffffffffffffffffffffffffffffff0',
+        characteristics: [
+          new bleno.Characteristic({
+            value: null,
+            uuid: 'fffffffffffffffffffffffffffffff1',
+            properties: ['read', 'write'],
+            onReadRequest: function(offset, callback) {
+              console.log('onReadRequest');
+            },
+            onWriteRequest: function(data, offset, withoutResponse, callback) {
+              console.log('onWriteRequest: ' + data.toString('hex') + ' ' + offset + ' ' + withoutResponse);
+            }
+          })
+        ]
+      })
+    ]);
   }
 });
